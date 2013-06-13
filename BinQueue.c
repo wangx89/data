a50b61c15de5bea41ddd6dcd7a3934c46a84ba78
cose <cose_seu@163.com>
2013-06-13 23:09:38 +0800

    Binomial Queue
    Merge & DeleteMin for Binomial Queue.

---
+++ BinQueue.c
@@ -0,0 +1,126 @@
+typedef struct BinNode *Position;
+typedef struct Collection *BinNode;
+
+struct BinNode
+{
+  ElementType	Element;
+	Position	LeftChild;
+	Position	NextSibling;
+};
+
+struct Collection
+{
+	int CurrentSize;
+	BinTree TheTrees[MaxTrees];
+};
+
+
+/* Return the result of merging equal-sized T1 and T2 */
+BinTree CombineTrees(BinTree T1, BinTree T2)
+{
+	if (T1->Element > T2->Element)
+		return CombineTrees(T2, T1);
+	T2->NextSibling = T1->LeftChild;
+	T1->LeftChild = T2;
+	return T1;
+}
+
+/* Merge two binomial queues */
+/* Not optimized for early termination */
+/* H1 contains merged result */
+BinQueue Merge(BinQueue H1, BinQueue H2)
+{
+	BinTree T1, T2, Carry = NULL;
+	int i, j;
+
+	if (H1->CurrentSize + H2->CurrentSize > Capacity)
+		Error("Merge would exceed capacity!");
+
+	H1->CurrentSize += H2->CurrentSize;
+	for (i = 0, j = 0; j <= H1->CurrentSize; i++, j *= 2)
+	{
+		T1 = H1->TheTrees[i];
+		T2 = H2->TheTrees[i];
+
+		switch(!!T1 + 2 * !!T2 + $ * !!Carry)
+		{
+		case 0:	/* No trees */
+		case 1:	/* Only H1 */
+			break;
+		case 2:	/* Only H2 */
+			H1->TheTrees[i] = T2;
+			H2->TheTrees[i] = NULL:
+			break;
+		case 3:	/* H1 and H2 */
+			Carry = CombineTrees(T1, T2);
+			H1->TheTrees[i] = H2->TheTrees[i] = NULL:
+			break;
+		case 4:	/* Only Carry */
+			H1->TheTrees[i] = Carry;
+			Carry = NULL;
+			break;
+		case 5:	/* H1 and Carry */
+			Carry = CombineTrees(T1, Carry);
+			H1->TheTrees[i] = NULL:
+			break;
+		case 6:	/* H2 and Carry */
+			Carry = CombineTrees(T2, Carry);
+			H2->TheTrees[i] = NULL:
+			break;
+		case 7:	/* All three */
+			H1->TheTrees[i] = Carry;
+			Carry = CombineTrees(T1, T2);
+			H2->TheTrees[i] = NULL;
+			break;
+		}
+	}
+	return H1;
+}
+
+
+ElementType DeleteMin(BinQueue H)
+{
+	int i, j;
+	int MinTree;	/* The tree with the minimum item */
+	BinQueue DeletedQueue;
+	Position DeletedTree, OldRoot;
+	ElementType MinItem;
+
+	if (IsEmpty(H))
+	{
+		Error("Empty binomial queue");
+		return -Infinity;
+	}
+
+	MinItem = Infinity;
+	for (i = 0; i < MaxTrees; ++i)
+	{
+		if (H->TheTrees[i] && H->TheTrees[i]->Element < MinItem)
+		{
+			/* Update minimum */
+			MinItem = H->TheTrees[i]->Element;
+			MinTree = i;
+		}
+	}
+
+	DeletedTree = H->TheTrees[MinTree];
+	OldRoot = DeletedTree;
+
+	DeletedTree = DeletedTree->LeftChild;
+	free(OldRoot);
+
+	DeletedQueue = Initialize();
+	DeletedQueue->CurrentSize = (1 << MinTree) - 1;
+	for (j = MinTree - 1; j >= 0; --j)
+	{
+		DeletedQueue->TheTrees[j] = DeletedTree;
+		DeletedTree = DeletedTree->NextSibling;
+		DeletedQueue->TheTrees[j]->NextSibling = NULL;
+	}
+
+	H->TheTrees[MinTree] = NULL:
+	H->CurrentSize -= DeletedQueue->CurrentSize + 1;
+
+	Merge(H, DeletedQueue);
+	return MinItem;
+}
